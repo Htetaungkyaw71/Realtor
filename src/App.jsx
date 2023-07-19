60cde4d47dfb5450acdef521ba7f4540ade03d39
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-07-19 19:11:42 +0630

    add code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import Home from "./components/Home";
-import Detail from "./components/Detail";
+import { Suspense, lazy } from "react";
+
+const Home = lazy(() => import("./components/Home"));
+const Detail = lazy(() => import("./components/Detail"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home itemsPerPage={8} />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="loader-container flex justify-center mt-28">
+            <div className="bouncing-text">
+              <div className="b">R</div>
+              <div className="o">E</div>
+              <div className="u">A</div>
+              <div className="n">L</div>
+              <div className="c">T</div>
+              <div className="e">O</div>
+              <div className="e">R</div>
+            </div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home itemsPerPage={8} />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
